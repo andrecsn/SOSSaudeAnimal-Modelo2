374fa86f8f81b73474d993626382ab7ac1556658
André <andrecsnogueira@gmail.com>
2015-07-08 17:59:48 -0300

    Atualização diária

---
+++ ClinicaVeterinaria4/Cadastros/RelatorioFinanceiro.aspx
@@ -0,0 +1,151 @@
+﻿<%@ Page Title="" Language="C#" MasterPageFile="~/Cadastros/Site1.Master" AutoEventWireup="true" CodeBehind="RelatorioFinanceiro.aspx.cs" Inherits="ClinicaVeterinaria.Cadastros.RelatorioFinanceiro" %>
+
+<asp:Content ID="Content1" ContentPlaceHolderID="Corpo" runat="server">
+
+    <form id="form1" runat="server">
+
+        <div class="row">
+            <div class="col-lg-12">
+                <h1 class="page-header">Relatório Financeiro - Pró-Labore</h1>
+            </div>
+        </div>
+
+        <div class="row">
+            <div class="col-lg-12">
+                <div class="panel panel-default">
+                    <div class="panel-heading"><b>Digite o intervalo de datas a serem relacionadas no relatório</b></div>
+                    <div class="panel-body">
+
+                        <div class="row">
+
+
+
+                            <div class='col-lg-3'>
+                                <div class="form-group">
+                                    <label>Data início: </label>
+                                    <div class='input-group date'>
+                                        <asp:TextBox ID="txtDt_inicio" runat="server" class="form-control data" required></asp:TextBox>
+                                        <span class="input-group-addon">
+                                            <span class="glyphicon glyphicon-calendar"></span>
+                                        </span>
+                                    </div>
+                                </div>
+                            </div>
+
+                            <div class='col-lg-3'>
+                                <div class="form-group">
+                                    <label>Data Fim: </label>
+                                    <div class='input-group date'>
+                                        <asp:TextBox ID="txtDt_fim" runat="server" class="form-control data" required></asp:TextBox>
+                                        <span class="input-group-addon">
+                                            <span class="glyphicon glyphicon-calendar"></span>
+                                        </span>
+                                    </div>
+                                </div>
+                            </div>
+
+                            <div class="col-lg-3">
+                                <div class="form-group">
+                                    </br>
+                                        <asp:Button ID="btnPesquisar" runat="server" Text="Pesquisar" class="btn btn-success" OnClick="btnPesquisar_Click" />
+                                    <asp:LinkButton ID="btnVoltar" runat="server" class="btn btn-primary" OnClick="btnVoltar_click">voltar</asp:LinkButton>
+                                </div>
+                            </div>
+
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+
+        <div class="row">
+            <div class="col-lg-3">
+            </div>
+            <div class="col-lg-5">
+                <table style="width: 100%;" class="table table-striped table-bordered">
+                    <tr>
+                        <td class="text-center">
+                            <b>Pró-Labore - De:
+                                <asp:Label ID="lblDt_inicio" runat="server"></asp:Label>
+                                Até
+                                <asp:Label ID="lblDt_Fim" runat="server"></asp:Label></b>
+                        </td>
+                    </tr>
+                    <tr>
+                        <td class="text-center">
+                            <b>Veterinária:
+                                <asp:Label ID="lblVeterinaria" runat="server"></asp:Label></b>
+                            <asp:HiddenField ID="hiddenCodigo" runat="server" />
+                        </td>
+                    </tr>
+                </table>
+            </div>
+        </div>
+
+        <div class="row">
+            <div class="col-lg-3">
+            </div>
+            <div class="col-lg-5">
+
+                <asp:Table ID="tblRelatorio" CssClass="table table-striped table-bordered" runat="server" />
+
+            </div>
+        </div>
+
+        <div class="row">
+            <div class="col-lg-3">
+            </div>
+            <div class="col-lg-5">
+
+                <table style="width: 100%;" class="table table-striped table-bordered">
+                    <tr class="text-center">
+                        <td><b>RECEBÍVEIS</b></td>
+                        <td><b>60%</b></td>
+                        <td><b>40%</b></td>
+                    </tr>
+                    <tr class="text-center">
+                        <td>
+                            <asp:Label ID="lblRecebiveis" runat="server"></asp:Label></td>
+                        <td>
+                            <asp:Label ID="lbl60" runat="server"></asp:Label></td>
+                        <td>
+                            <asp:Label ID="lbl40" runat="server"></asp:Label></td>
+                    </tr>
+                </table>
+
+            </div>
+        </div>
+
+
+        <div class="row">
+            <div class="col-lg-3">
+            </div>
+            <div class="col-lg-5">
+
+                <table style="width: 100%;" class="table table-bordered">
+                    <tr>
+                        <td><b>TOTAL DE ENTRADAS</b></td>
+                        <td>
+                            <asp:Label ID="Label4" runat="server"></asp:Label></td>
+                    </tr>
+                    <tr>
+                        <td>
+                            <b>TOTAL CARTÃO JÁ RECEBIDO</b></td>
+                        <td>
+                            <asp:Label ID="Label2" runat="server"></asp:Label></td>
+                    </tr>
+                    <tr>
+                        <td>
+                            <b>TOTAL A RECEBER</b></td>
+                        <td>
+                            <asp:Label ID="Label5" runat="server"></asp:Label></td>
+                    </tr>
+                </table>
+
+            </div>
+        </div>
+
+    </form>
+
+
+</asp:Content>
---
+++ ClinicaVeterinaria4/Cadastros/RelatorioFinanceiro.aspx.cs
@@ -0,0 +1,248 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity.Core.Objects;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace ClinicaVeterinaria.Cadastros
+{
+    public partial class RelatorioFinanceiro : Business.Prontuario_Business
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            if (!this.IsPostBack)
+            {
+
+            }
+        }
+
+        protected void btnPesquisar_Click(object sender, EventArgs e)
+        {
+            DateTime dt_inicio = Convert.ToDateTime(txtDt_inicio.Text);
+            DateTime dt_fim = Convert.ToDateTime(txtDt_fim.Text);
+
+            listarCabecalho(dt_inicio, dt_fim);
+            gerarRelatorio(dt_inicio, dt_fim);
+
+        }
+
+        private void listarCabecalho(DateTime dt_inicio, DateTime dt_fim)
+        {
+            int codigo = Convert.ToInt32(HttpContext.Current.Session["cd_funcionario"]);
+            hiddenCodigo.Value = codigo.ToString();
+
+            Models.funcionario veterinaria = contexto.funcionario.First(x => x.cd_funcionario == codigo);
+            lblVeterinaria.Text = veterinaria.nm_funcionario;
+
+            lblDt_inicio.Text = dt_inicio.ToShortDateString();
+            lblDt_Fim.Text = dt_fim.ToShortDateString();
+
+            //Session.Remove("cd_funcionario");
+        }
+
+        private void gerarRelatorio(DateTime Dt_inicio, DateTime Dt_fim)
+        {
+            tblRelatorio.Controls.Clear();
+
+            int linhas = Convert.ToInt32(Dt_fim.Subtract(Dt_inicio).TotalDays);
+            DateTime data = Dt_inicio;
+
+            double dinheiro = 0;
+            double debito = 0;
+            double credito = 0;
+
+            for (int i = 0; i <= linhas; i++)
+            {
+                TableRow novaLinha = new TableRow();
+                tblRelatorio.Controls.Add(novaLinha);
+
+                for (int j = 0; j < 4; j++)
+                {
+                    TableCell novaColuna = new TableCell();
+                    Label lblNew = new Label();
+
+                    if (j == 0)
+                    {
+                        lblNew.Text = data.ToShortDateString();
+                        novaColuna.Controls.Add(lblNew);
+                        novaLinha.Controls.Add(novaColuna);
+                    }
+
+                    if (j == 1)
+                    {
+                        lblNew.Text = calculoDinheiro(data).ToString();
+                        novaColuna.Controls.Add(lblNew);
+                        novaLinha.Controls.Add(novaColuna);
+                    }
+
+                    if (j == 2)
+                    {
+                        lblNew.Text = calculoDebito(data).ToString();
+                        novaColuna.Controls.Add(lblNew);
+                        novaLinha.Controls.Add(novaColuna);
+                    }
+
+                    if (j == 3)
+                    {
+                        lblNew.Text = calculoCredito(data).ToString();
+                        novaColuna.Controls.Add(lblNew);
+                        novaLinha.Controls.Add(novaColuna);
+                    }
+                }
+
+                //calculando total das celulas
+                dinheiro = dinheiro + calculoDinheiro(data);
+                debito = debito + calculoDebito(data);
+                credito = credito + calculoCredito(data);
+
+                data = data.AddDays(1);
+            }
+
+
+            for (int i = 0; i < 1; i++)
+            {
+                TableRow novaLinha = new TableRow();
+                tblRelatorio.Controls.Add(novaLinha);
+
+                for (int j = 0; j < 4; j++)
+                {
+                    TableCell novaColuna = new TableCell();
+                    Label lblNew = new Label();
+
+                    if (j == 0)
+                    {
+                        lblNew.Text = "<b>TOTAL SEM DESCONTO</b>";
+                        novaColuna.Controls.Add(lblNew);
+                        novaLinha.Controls.Add(novaColuna);
+                    }
+
+                    if (j == 1)
+                    {
+                        lblNew.Text = dinheiro.ToString();
+                        novaColuna.Controls.Add(lblNew);
+                        novaLinha.Controls.Add(novaColuna);
+                    }
+
+                    if (j == 2)
+                    {
+                        lblNew.Text = debito.ToString();
+                        novaColuna.Controls.Add(lblNew);
+                        novaLinha.Controls.Add(novaColuna);
+                    }
+
+                    if (j == 3)
+                    {
+                        lblNew.Text = credito.ToString();
+                        novaColuna.Controls.Add(lblNew);
+                        novaLinha.Controls.Add(novaColuna);
+                    }
+                }
+            }
+
+
+
+            for (int i = 0; i < 1; i++)
+            {
+                TableRow novaLinha = new TableRow();
+                tblRelatorio.Controls.Add(novaLinha);
+
+                for (int j = 0; j < 4; j++)
+                {
+                    TableCell novaColuna = new TableCell();
+                    Label lblNew = new Label();
+
+                    if (j == 0)
+                    {
+                        lblNew.Text = "<b>TOTAL COM DESCONTO</b>";
+                        novaColuna.Controls.Add(lblNew);
+                        novaLinha.Controls.Add(novaColuna);
+                    }
+
+                    if (j == 1)
+                    {
+                        lblNew.Text = dinheiro.ToString();
+                        novaColuna.Controls.Add(lblNew);
+                        novaLinha.Controls.Add(novaColuna);
+                    }
+
+                    if (j == 2)
+                    {
+                        lblNew.Text = debito.ToString();
+                        novaColuna.Controls.Add(lblNew);
+                        novaLinha.Controls.Add(novaColuna);
+                    }
+
+                    if (j == 3)
+                    {
+                        lblNew.Text = credito.ToString();
+                        novaColuna.Controls.Add(lblNew);
+                        novaLinha.Controls.Add(novaColuna);
+                    }
+                }
+            }
+
+            lblRecebiveis.Text = string.Format("{0:C}", (dinheiro + debito + credito));
+            lbl60.Text = string.Format("{0:C}", ((dinheiro + debito + credito) * 0.6));
+            lbl40.Text = string.Format("{0:C}", ((dinheiro + debito + credito) * 0.4));
+
+        }
+
+        private double calculoDinheiro(DateTime data)
+        {
+            int funcionario = Convert.ToInt32(hiddenCodigo.Value);
+
+            string data2 = data.ToShortDateString();
+
+            var dados = from a in contexto.consulta
+                        where a.cd_funcionario == funcionario
+                        & EntityFunctions.TruncateTime(a.dt_consulta) == data
+                        select new
+                        {
+                            dinheiro = a.pg_dinheiro
+                        };
+
+            return Convert.ToDouble(dados.Sum(soma => soma.dinheiro));
+        }
+
+        private double calculoDebito(DateTime data)
+        {
+            int funcionario = Convert.ToInt32(hiddenCodigo.Value);
+
+            string data2 = data.ToShortDateString();
+
+            var dados = from a in contexto.consulta
+                        where a.cd_funcionario == funcionario
+                        & EntityFunctions.TruncateTime(a.dt_consulta) == data
+                        select new
+                        {
+                            dinheiro = a.pg_debito
+                        };
+
+            return Convert.ToDouble(dados.Sum(soma => soma.dinheiro));
+        }
+
+        private double calculoCredito(DateTime data)
+        {
+            int funcionario = Convert.ToInt32(hiddenCodigo.Value);
+
+            string data2 = data.ToShortDateString();
+
+            var dados = from a in contexto.consulta
+                        where a.cd_funcionario == funcionario
+                        & EntityFunctions.TruncateTime(a.dt_consulta) == data
+                        select new
+                        {
+                            dinheiro = a.pg_credito
+                        };
+
+            return Convert.ToDouble(dados.Sum(soma => soma.dinheiro));
+        }
+
+        protected void btnVoltar_click(object sender, EventArgs e)
+        {
+            Server.Transfer("ListarFinanceiro.aspx");
+        }
+    }
+}
